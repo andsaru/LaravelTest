0458d26bba7422cd01d3437435c84ed252e467a9
andsaru <andsaru@hotmail.es>
2021-11-22 01:00:09 +0100

    creating navbar with route

--- LaravelTest/resources/views/layouts/app.blade.php
+++ LaravelTest/resources/views/layouts/app.blade.php
@@ -11,6 +11,20 @@
   <!-- ... --->
   <body class="bg-gray-100 text-gary-800">
 
+    <nav class="flex py-5 bg-indigo-500 text-white">
+      <div class="w-1/2 px-12 mr-auto">
+        <p class="text-2xl font-bold">Laravel Test</p>
+      </div>
+
+      <ul class="w-1/2 px-16 ml-auto flex justify-end pt-1">
+        <li>
+          <a href="{{ route('login.index') }}" class="font-semibolt 
+          hover:bg-indigo-700 py-3 px-4 rounded-md">Log In</a>
+        </li>
+      </ul>
+
+    </nav>
+
     @yield('content')
 
 
